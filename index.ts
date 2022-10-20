e87f4f9bbfecdc06f0d4925d5f498ef0ab147a96
hai.pham <haiphamtk13clc@gmail.com>
2022-10-20 21:34:56 +0700

    init prisma

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
 import express, { Express } from "express";
-import dotenv from "dotenv";
+import { appPort } from "./config";
 
 const app: Express = express();
-
-dotenv.config();
-
-const appPort = process.env.APP_PORT || 7200;
 
 app.listen(appPort, () => {
   console.log(`Congratulation! App is listening on port ${appPort}`);
