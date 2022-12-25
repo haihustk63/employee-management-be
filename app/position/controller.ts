7eb6568c60770d370b7b766985392f48a91df3be
haihustk63 <haiphamtk13clc@gmail.com>
2022-12-26 00:55:37 +0700

    update and delete position

--- app/position/controller.ts
+++ app/position/controller.ts
@@ -22,4 +22,37 @@ const getAllPositions = async (req: Request, res: Response) => {
   }
 };
 
-export { createNewPosition, getAllPositions };
+const updatePosition = async (req: Request, res: Response) => {
+  try {
+    const { positionId } = req.params;
+    const { data } = req.body;
+
+    const updatedPosition = await prisma.position.update({
+      where: {
+        id: Number(positionId),
+      },
+      data,
+    });
+    return res.status(200).send({ updatedPosition });
+  } catch (error: any) {
+    console.log(error)
+    return res.sendStatus(400);
+  }
+};
+
+const deletePosition = async (req: Request, res: Response) => {
+  try {
+    const { positionId } = req.params;
+
+    await prisma.position.delete({
+      where: {
+        id: Number(positionId),
+      },
+    });
+    return res.status(200).send("OK");
+  } catch (error: any) {
+    return res.sendStatus(400);
+  }
+};
+
+export { createNewPosition, getAllPositions, updatePosition, deletePosition };
--- app/position/route.ts
+++ app/position/route.ts
@@ -1,13 +1,22 @@
 import { Router } from "express";
 import { ROUTES } from "constants/index";
-import { createNewPosition, getAllPositions } from "./controller";
+import {
+  createNewPosition,
+  getAllPositions,
+  updatePosition,
+  deletePosition,
+} from "./controller";
 
-const { POSITION } = ROUTES;
+const { POSITION, POSITION_MODIFY } = ROUTES;
 
 const router = Router();
 
 router.get(POSITION, getAllPositions);
 
 router.post(POSITION, createNewPosition);
+
+router.patch(POSITION_MODIFY, updatePosition);
+
+router.delete(POSITION_MODIFY, deletePosition);
 
 export default router;
