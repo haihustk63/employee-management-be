38dbb55387467ed7440d58cf76a4d68686d58f47
haihustk63 <haiphamtk13clc@gmail.com>
2023-02-13 23:14:59 +0700

    :+1: Modify commands

--- dist/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.transformFileName = void 0;
-const transformFileName = (fileName) => {
-    return fileName.replace(/[ ]+/g, "-");
-};
-exports.transformFileName = transformFileName;
